89bf33189f33a112325c6299b31c6e0334e4cbce
Perennial2002 <pendragon05082002@gmail.com>
2022-01-28 20:33:18 +0500

    Initial commit

---
+++ app/src/main/java/com/example/edittextkeylistenerjava/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.edittextkeylistenerjava;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText editText;
+    private TextView textView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initViews();
+    }
+
+    private void initViews() {
+        editText = findViewById(R.id.edt_edit_text);
+        textView = findViewById(R.id.tv_text_view);
+
+        textChangedListener();
+
+        onKeyListener();
+    }
+
+    //this function detects whether enter button of keyboard is pressed!
+    private void onKeyListener() {
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    Toast.makeText(getApplicationContext(), "Enter pressed", Toast.LENGTH_SHORT).show();
+                }
+                return false;
+            }
+        });
+    }
+
+    private void textChangedListener() {
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) { }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                textView.setText(s.toString());
+            }
+        });
+    }
+}
